e189a548ee2b04f2408417d95ca73b58fbe73153
tanujbhatia24 <tanujbhatia0001@gmail.com>
2025-02-22 19:17:32 +0530

    Update DEV Branch

--- deploy_setup/check_commits.py
+++ deploy_setup/check_commits.py
@@ -10,8 +10,8 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")  # Use a GitHub token with repo read ac
 
 # Get the current user's home directory dynamically
 HOME_DIR = os.path.expanduser("~")
-LOCAL_COMMIT_FILE = f"{HOME_DIR}/Devops_CICD/log/latest_commit.txt"
-DEPLOY_SCRIPT = f"{HOME_DIR}/Devops_CICD/deploy.sh"
+LOCAL_COMMIT_FILE = f"{HOME_DIR}/Devops_pipeline/log/latest_commit.txt"
+DEPLOY_SCRIPT = f"{HOME_DIR}/Devops_pipeline/deploy.sh"
 
 def get_latest_commit():
     url = f"https://api.github.com/repos/{GITHUB_REPO}/commits/main"
--- deploy_setup/deploy.sh
+++ deploy_setup/deploy.sh
@@ -2,7 +2,7 @@
 
 # Get the current user's home directory
 USER_HOME=$(eval echo ~$(whoami))
-PROJECT_DIR="$USER_HOME/Devops_CICD/log"
+PROJECT_DIR="$USER_HOME/Devops_pipeline/log"
 
 # Log start
 echo "Starting deployment at $(date)" >> $PROJECT_DIR/deploy.log
