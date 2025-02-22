bead4b6dd5634c07bc7fff7589ff7a64f294a827
tanujbhatia24 <tanujbhatia0001@gmail.com>
2025-02-22 21:38:08 +0530

    Update main Branch

--- deploy_sample/check_commits.py
+++ deploy_sample/check_commits.py
@@ -10,7 +10,7 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")  # Use a GitHub token with repo read ac
 
 # Get the current user's home directory dynamically
 HOME_DIR = os.path.expanduser("~")
-LOCAL_COMMIT_FILE = f"{HOME_DIR}/Devops_CICD/Devops_pipeline/log/latest_commit.txt"
+LOCAL_COMMIT_FILE = f"{HOME_DIR}/Devops_CICD/log/latest_commit.txt"
 DEPLOY_SCRIPT = f"{HOME_DIR}/Devops_CICD/Devops_pipeline/deploy.sh"
 
 def get_latest_commit():
